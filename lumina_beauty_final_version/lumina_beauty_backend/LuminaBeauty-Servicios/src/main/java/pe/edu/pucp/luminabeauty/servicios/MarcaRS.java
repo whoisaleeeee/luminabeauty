9b57699dd71c010a26fd1ecc71b25badd14f89fc
Ememoraa <a20223237@pucp.edu.pe>
2026-06-24 03:43:59 -0500

    Push rapido

---
+++ lumina_beauty_final_version/lumina_beauty_backend/LuminaBeauty-Servicios/src/main/java/pe/edu/pucp/luminabeauty/servicios/MarcaRS.java
@@ -0,0 +1,5 @@
+package pe.edu.pucp.luminabeauty.servicios;
+
+
+public class MarcaRS {
+}
--- proyectos-anteriores/SoluLumina/SoluLumina/Models/Marca.cs
+++ proyectos-anteriores/SoluLumina/SoluLumina/Models/Marca.cs
@@ -1,9 +1,29 @@
+using Newtonsoft.Json;
+
 namespace SoluLumina.Models;
 
 public class Marca
 {
-    public int Id { get; set; }
-    public string Nombre { get; set; } = string.Empty;
-    public string Descripcion { get; set; } = string.Empty;
-    public string LogoUrl { get; set; } = string.Empty;
+    private int id_marca;
+    private String nombre;
+    private String descripcion;
+    private String logo_url;
+    private int estado;
+    private DateTime fecha_creacion;
+    private DateTime fecha_actualizacion;
+
+    [JsonProperty("id_marca")]
+    public int Id_marca { get => id_marca; set => id_marca = value; }
+    [JsonProperty("nombre")]
+    public string Nombre { get => nombre; set => nombre = value; }
+    [JsonProperty("descripcion")]
+    public string Descripcion { get => descripcion; set => descripcion = value; }
+    [JsonProperty("logo_url")]
+    public string Logo_url { get => logo_url; set => logo_url = value; }
+    [JsonProperty("estado")]
+    public int Estado { get => estado; set => estado = value; }
+    [JsonProperty("fecha_creacion")]
+    public DateTime Fecha_creacion { get => fecha_creacion; set => fecha_creacion = value; }
+    [JsonProperty("fecha_actualizacion")]
+    public DateTime Fecha_actualizacion { get => fecha_actualizacion; set => fecha_actualizacion = value; }
 }
