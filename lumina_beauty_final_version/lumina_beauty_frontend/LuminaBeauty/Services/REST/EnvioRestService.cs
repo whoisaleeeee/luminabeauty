8a6b989aeb8afbe52d3758ae4528ad50f344cab8
alear <alexandra.arrobas@pucp.edu.pe>
2026-06-25 15:02:30 -0500

    add EnvioRestService,cs

---
+++ lumina_beauty_final_version/lumina_beauty_frontend/LuminaBeauty/Services/REST/EnvioRestService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Json;
+using LuminaBeauty.Servicios.Modelo;
+
+namespace LuminaBeauty.Servicios.REST
+{
+    public class EnvioRestService
+    {
+        private readonly HttpClient _http;
+
+        public EnvioRestService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<Envio?> BuscarPorPedidoAsync(int idPedido)
+        {
+            try
+            {
+                using var response = await _http.GetAsync(
+                    $"webresources/EnvioRS/buscarPorPedido/{idPedido}"
+                );
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+
+                return await response.Content.ReadFromJsonAsync<Envio>();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
