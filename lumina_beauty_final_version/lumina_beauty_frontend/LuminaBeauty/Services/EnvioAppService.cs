498bc81fd820bb35a460de41e09d19725ca34869
alear <alexandra.arrobas@pucp.edu.pe>
2026-06-25 15:04:08 -0500

    add EnvioAppService.cs

---
+++ lumina_beauty_final_version/lumina_beauty_frontend/LuminaBeauty/Services/EnvioAppService.cs
@@ -0,0 +1,20 @@
+﻿using LuminaBeauty.Servicios.Modelo;
+using LuminaBeauty.Servicios.REST;
+
+namespace LuminaBeauty.Services
+{
+    public class EnvioAppService
+    {
+        private readonly EnvioRestService _envioRestService;
+
+        public EnvioAppService(EnvioRestService envioRestService)
+        {
+            _envioRestService = envioRestService;
+        }
+
+        public Task<Envio?> BuscarPorPedidoAsync(int idPedido)
+        {
+            return _envioRestService.BuscarPorPedidoAsync(idPedido);
+        }
+    }
+}
